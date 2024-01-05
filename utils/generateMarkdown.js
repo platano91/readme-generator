a69ef46a213ad0dcba0dbb892af0e0501a8497f5
Samuel <salvadomo@hotmail.com>
2024-01-05 13:39:24 -0700

    license render function

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -13,9 +13,12 @@ function renderLicenseLink(license) {
   return `https://opensource.org/licenses/${license}`;
 }
 
-// TODO: Create a function that returns the license section of README
+// function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (!license || license === 'None') return '';
+  return `## License\nThis project is licensed under the ${license} license.`;
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
